597537875d0a9bd8e3b390f68e60122a58f7339b
Inyong Hwang <hwanginyong@seoultech.ac.kr>
2024-10-15 12:15:47 +0900

    Initial commit

---
+++ src/test/java/itm/CalculatorTests.java
@@ -0,0 +1,43 @@
+package itm;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import itm.oss.Calculator;
+
+
+
+@DisplayName("Calculator tests for OSS class")
+
+public class CalculatorTests {
+    private final Calculator calc = new Calculator();
+    @Test
+    @DisplayName("Test for add function.")
+    void add(){
+        assertEquals(10, calc.add(5,5), "Expected value is 10 in add function");
+
+    }
+
+    @Test
+    @DisplayName("Testing function isPrime.")
+    void isfivePrime(){
+        assertTrue(calc.isPrime(5), "5 is prime.");
+    }    
+
+    @Test
+    @DisplayName("Testing function isPrime (4).")
+    void isfourPrime(){
+        assertFalse(calc.isPrime(4), "4 is not prime.");
+    }
+
+    @Test
+    @DisplayName("Testing function isPrime (0).")
+    void iszeroNotPrime(){
+        assertFalse(calc.isPrime(0), "0 is not prime.");
+    }
+}
